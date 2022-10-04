6ff073669cfbb87e0aec369114f5408db03c7a6f
jia <jiabids@gmail.com>
2022-10-03 22:56:30 -0700

    add grid UI

--- javascripts.js
+++ javascripts.js
@@ -1,12 +1,13 @@
 const container = document.getElementById("container");
 
 function makeGrid(number){
-    container.style.setProperty("grid-rows", number);
-    container.style.setProperty("grid-column", number);
+    container.style.setProperty("--grid-row", number);
+    container.style.setProperty("--grid-column", number);
 
-    for(i = 0; i < (number**2); i++){
+    for(i = 0; i < (number * number); i++){
         let cell = document.createElement("div");
-        cell.innerText = i;
         container.appendChild(cell).className = "grid-item";
     }
 }
+
+makeGrid(16);
