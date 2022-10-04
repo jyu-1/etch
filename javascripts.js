cc06b106cb188e8985646294b2a1238e278a288e
jia <jiabids@gmail.com>
2022-10-04 12:51:07 -0700

    add color picker

--- javascripts.js
+++ javascripts.js
@@ -1,10 +1,16 @@
 const container = document.getElementById("container");
 const slider = document.getElementById("myslider");
 const output = document.getElementById("output");
+const colorPicker = document.getElementById("colorpicker");
+let currentColor = "#000000";
+
+colorPicker.addEventListener("change", (event)=>{
+    currentColor = event.target.value;
+})
 
 output.innerText = slider.value + " x " + slider.value;
 
-slider.oninput = function (){
+slider.onchange = function (){
     output.innerText = slider.value + " x " + slider.value;
     resetGrid();
     makeGrid(slider.value);
@@ -17,7 +23,7 @@ function makeGrid(number) {
     for (i = 0; i < (number * number); i++) {
         const cell = document.createElement("div");
         cell.addEventListener("mouseover", (event) => {
-            event.target.style.backgroundColor = "black";
+            event.target.style.backgroundColor = currentColor;
         })
         container.appendChild(cell).classList.add("grid-item");
     }
