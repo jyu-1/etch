dd46ee8eca3f6a23910f83ae18716d21007b1685
jia <jiabids@gmail.com>
2022-10-04 13:46:00 -0700

    add color buttons and dynamic slider color

--- javascripts.js
+++ javascripts.js
@@ -1,16 +1,35 @@
 const container = document.getElementById("container");
 const slider = document.getElementById("myslider");
 const output = document.getElementById("output");
-const colorPicker = document.getElementById("colorpicker");
-let currentColor = "#000000";
 
-colorPicker.addEventListener("change", (event)=>{
+let currentColor = "#000000";
+let currentMode = "default";
+let randomRed = Math.floor(Math.random() * 256);
+let randomGreen = Math.floor(Math.random() * 256);
+let randomBlue = Math.floor(Math.random() * 256);
+
+slider.style.setProperty("--slidercolor", currentColor);
+
+const colorButton = document.getElementById("colorbutton").addEventListener("click", ()=>{
+    currentMode = "default";
+    resetGrid();
+    makeGrid(slider.value);
+})
+
+const rainbowButton = document.getElementById("rainbowbutton").addEventListener("click", ()=>{
+    currentMode = "rainbow";
+    resetGrid();
+    makeGrid(slider.value);
+})
+
+const colorPicker = document.getElementById("colorpicker").addEventListener("change", (event) => {
     currentColor = event.target.value;
+    slider.style.setProperty("--slidercolor", currentColor);
 })
 
 output.innerText = slider.value + " x " + slider.value;
 
-slider.onchange = function (){
+slider.onchange = function () {
     output.innerText = slider.value + " x " + slider.value;
     resetGrid();
     makeGrid(slider.value);
@@ -19,19 +38,34 @@ slider.onchange = function (){
 function makeGrid(number) {
     container.style.setProperty("--grid-row", number);
     container.style.setProperty("--grid-column", number);
+    if (currentMode == "default") {
+        for (i = 0; i < (number * number); i++) {
+            const cell = document.createElement("div");
+            cell.addEventListener("mouseover", (event) => {
+                event.target.style.backgroundColor = currentColor;
+            })
+            container.appendChild(cell).classList.add("grid-item");
+        }
+    }
+    else if (currentMode == "rainbow") {
+        for (i = 0; i < (number * number); i++) {
 
-    for (i = 0; i < (number * number); i++) {
-        const cell = document.createElement("div");
-        cell.addEventListener("mouseover", (event) => {
-            event.target.style.backgroundColor = currentColor;
-        })
-        container.appendChild(cell).classList.add("grid-item");
+            const cell = document.createElement("div");
+            cell.addEventListener("mouseover", (event) => {
+                randomRed = Math.floor(Math.random() * 256);
+                randomGreen = Math.floor(Math.random() * 256);
+                randomBlue = Math.floor(Math.random() * 256);
+                event.target.style.backgroundColor = `rgb(${randomRed}, ${randomGreen}, ${randomBlue})`;
+                slider.style.setProperty("--slidercolor", `rgb(${randomRed}, ${randomGreen}, ${randomBlue})`);
+            })
+            container.appendChild(cell).classList.add("grid-item");
+        }
     }
 }
 
 makeGrid(slider.value);
 
-function resetGrid(){
+function resetGrid() {
     container.style.setProperty("--grid-row", "none");
     container.style.setProperty("--grid-column", "none");
 
